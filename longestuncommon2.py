59593c6bd3d3f3d3db8c21d716b332a001df4cb1
MasayoshiTsutsui <tsutsui-masayoshi0528@g.ecc.u-tokyo.ac.jp>
2023-02-21 22:23:36 +0900

    new

--- longestuncommon2.py
+++ longestuncommon2.py
@@ -1,4 +1,38 @@
 from typing import List
 
 class Solution:
+    def issubsequence(self, base: str, comp: str) -> bool:
+        if len(base) > len(comp):
+            return False
+        baseIdx = 0
+        for c in comp:
+            if base[baseIdx] == c:
+                baseIdx += 1
+                if baseIdx == len(base):
+                    return True
+        return False
+
     def findLUSlength(self, strs: List[str]) -> int:
+        strs.sort(key=len, reverse=True)
+        for i, s in enumerate(strs):
+            if all(not self.issubsequence(s, t) for j, t in enumerate(strs) if i != j):
+                return len(s)
+        return -1
+
+strs = ["aaa","aaa","aa"]
+print(Solution().findLUSlength(strs))
+
+"""
+aaa, aaa, c -> c
+aaa, aba, ab -> aaa
+aaa, aba, aba -> aaa
+
+aba, aba, ab, bc
+if len is all same:
+    return len(strs[i]) or -1
+else:
+
+
+
+"""
+
